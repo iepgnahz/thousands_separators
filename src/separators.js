c699c7ddbe756a81ea03dd652a7d83d6edd41399
zhangpei <zhangpei@zhangpeideMacBook-Pro.local>
2016-05-30 22:14:28 +0800

    thousands_separators finish

--- src/separators.js
+++ src/separators.js
@@ -1,7 +1,22 @@
 'use strict';
 
 function thousands_separators(num) {
-  
+  var numStr = "" + num;
+  var nums = numStr.split(".");
+  var integer = nums[0];
+  var decimal = nums[1];
+  return splitNum(integer) + (decimal ? "." + decimal : "");
+}
+
+function splitNum(numStr){
+  var sp = "";
+  var sl = "";
+  for(var i = numStr.length - 3; i > 0; i = i - 3){
+    sp = numStr.substring(0,i);
+    sl = numStr.substring(i);
+    numStr = sp + "," + sl;
+  }
+  return numStr;
 }
 
 module.exports = thousands_separators;
